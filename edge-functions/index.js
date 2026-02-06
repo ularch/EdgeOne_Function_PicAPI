3c1d55edf50c7a7bba3fc1d76fd4f81d4b109bf5
Harold Lach Lei <60141262+ularch@users.noreply.github.com>
2026-02-06 19:07:59 +0800

    更新 index.js

--- edge-functions/index.js
+++ edge-functions/index.js
@@ -40,7 +40,7 @@ async function handleRequest(request) {
     }
 
     const maxHorizontalImageNumber = 9; // 横屏
-    const maxVerticalImageNumber = 8; // 竖屏
+    const maxVerticalImageNumber = 10; // 竖屏
 
     if (imgType === 'h') {  // 横屏
       // 生成1到maxHorizontalImageNumber之间的随机数
